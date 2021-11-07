df1dcfa12498a52ab9e3a41a5d7c1d407783d716
kncalderong <kncalderong@gmail.com>
2021-11-06 22:03:57 -0500

    TaskModel mongoose with comments

--- taskManager/models/Task.js
+++ taskManager/models/Task.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
     name:{
         type:String,
-        required:[true,"provide a name"],
+        required:[true,"provide a name"], //the second argument is the err message provided
         trim: true,
         maxlength:[30,"limit of 30 characters exceeded"]
     },
